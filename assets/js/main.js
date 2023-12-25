967fd34d64b564d047b64a83e726b83dce2249da
dineshdevelope <dineshindevelope@gmail.com>
2023-12-26 01:49:34 +0530

    feat:apply the localStorage Value in UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,6 +74,8 @@ validateForm.addField(
   }
 );
 
+const localStorageKey = "courierData"; //keyName For Getting And Setting localStorage
+
 //console.log(validateForm);
 validateForm.onSuccess(() => {
   const formData = new FormData(formEl);
@@ -84,7 +86,7 @@ validateForm.onSuccess(() => {
   const newCourierData = [];
 
   //Get my existing LocalStorage Value
-  const existingCourierData = localStorage.getItem("courierData"); //get Local Storage
+  const existingCourierData = localStorage.getItem(localStorageKey); //get Local Storage
   const existingCourierArray = JSON.parse(existingCourierData); //string to javascript
 
   if (existingCourierArray) {
@@ -92,11 +94,54 @@ validateForm.onSuccess(() => {
     //console.log(existingCourierArray);
 
     //LocalStorage
-    localStorage.setItem("courierData", JSON.stringify(existingCourierArray));
+    localStorage.setItem(localStorageKey, JSON.stringify(existingCourierArray));
   } else {
     newCourierData.push(formObj);
-    localStorage.setItem("courierData", JSON.stringify(newCourierData));
+    localStorage.setItem(localStorageKey, JSON.stringify(newCourierData));
   }
   alert("Courier Request Submitted Successfully...!");
   formEl.reset();
 });
+
+function getAllCourierDatas() {
+  // Get All Stored Courier datas which are available in localStorage
+  const courierData = localStorage.getItem(localStorageKey);
+
+  const courierDataArr = JSON.parse(courierData);
+  //console.log(courierDataArr);
+
+  if (courierDataArr) {
+    //write those value in UI
+    const tableEl = document.getElementById("courierDataTable");
+
+    const finalData = courierDataArr
+      .map((courierData) => {
+        const newTrTemplate = `
+
+  <tr>
+  <td class="px-2 py-1 border">${courierData.name}</td>
+  <td class="px-2 py-1 border">${courierData.mobile}</td>
+  <td class="px-2 py-1 border">${courierData["pickup-date"]}</td>
+   <td class="px-2 py-1 border">${courierData["pickup-area"]}</td>
+  <td class="px-2 py-1 border">
+                <button
+                  type="button"
+                  class="px-2 py-1 rounded bg-red-500 hover:bg-red-600 text-white text-sm"
+                >
+                  Delete
+                </button>
+              </td>
+  </tr>
+  `;
+
+        return newTrTemplate;
+      })
+      .join(" ");
+
+    tableEl.innerHTML += finalData;
+  } else {
+    console.log("No Value Available on localStorage");
+  }
+}
+
+getAllCourierDatas();
